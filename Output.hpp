a86562695a91904abb397432afdb0c34aa5f2cf6
Chigric <ggggg.writer.99@mail.ru>
2020-11-28 17:07:39 +0300

    more pretty output (only 10 columns in each row)

--- Output.hpp
+++ Output.hpp
@@ -65,22 +65,30 @@ template <typename Vector>
 void output2File(std::fstream& file, Vector vecX, Vector vecY)
 {
     DoubleOutput out(file);
+    size_t size = vecX.size();
+    const size_t prettyOutput = 10;
+    auto myLoop = [&out, &prettyOutput] (const char* str, Vector& vec) {
+        out << str;
+        for (size_t i = 0; i < prettyOutput; i++)
+            out << vec[i] << " ";
+    };
 
-    // out array of i
-    file << "i ";   std::cout << "i ";
-    for (size_t i = 0; i < vecX.size(); i++)
-        out << std::to_string(i) << " ";
-    out << SundayWork::endl;
-    // out array of X_i
-    file << "x_i ";   std::cout << "x_i ";
-    for (auto& ptr : vecX) {
-        out << ptr << " ";
+    size_t tmpSize = 0;
+    while (tmpSize < size) {
+        // out array of i
+        out << "i ";
+        for (size_t i = 0; i < prettyOutput; i++)
+            out << i << " ";
+        out << SundayWork::endl;
+        // out array of x_i
+        myLoop("x_i ", vecX);
+        out << SundayWork::endl;
+        // out array of Y_i
+        myLoop("y_i ", vecY);
+
+        if ((tmpSize += prettyOutput) < size)
+            out << SundayWork::endl;
     }
-    out << SundayWork::endl;
-    // out array of Y_i
-    file << "y_i ";   std::cout << "y_i ";
-    for (auto& ptr : vecY)
-        out << ptr << " ";
 }
 
 template <typename Vector>
